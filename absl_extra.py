58d31e19e9401cddc20817679f6441836eabc210
artem-sereda <77972398+artem-sereda@users.noreply.github.com>
2023-04-06 11:47:56 +0200

    *

--- absl_extra.py
+++ absl_extra.py
@@ -1,9 +1,29 @@
-from __future__ import annotations
-
-from typing import Optional
-
+import json
+from typing import Callable, List, Optional, TypeVar
+from operator import attrgetter
+from absl import app, flags, logging
+from ml_collections import ConfigDict, config_flags
+from functools import wraps
+from dotenv import load_dotenv
 import slack_sdk
-from absl import logging
+
+MainFn = Callable[[List[str], Optional[ConfigDict]], None]
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+def value_or_default(value: T, default_factory: Callable[[], T]) -> T:
+    if value is not None:
+        return value
+
+    return default_factory()
+
+
+def map_optional(value: Optional[T], fn: Callable[[T], R]) -> Optional[R]:
+    if value is None:
+        return None
+    return fn(value)
 
 
 class Notifier:
@@ -86,3 +106,52 @@ class SlackNotifier(Notifier):
             text="Job Finished!",
         )
         raise ex
+
+
+class ExceptionHandlerImpl(app.ExceptionHandler):
+    def __init__(self, cmd: str, notifier: Notifier):
+        self.cmd = cmd
+        self.notifier = notifier
+
+    def handle(self, exc: Exception):
+        self.notifier.notify_job_failed(self.cmd, exc)
+
+
+class App:
+    def __init__(
+        self,
+        *,
+        name: Optional[str] = None,
+        notifier: Optional[Notifier] = None,
+        config_file: Optional[str] = None,
+        env_file: Optional[str] = None,
+    ):
+        self.name = value_or_default(name, lambda: "app")
+        self.notifier = value_or_default(notifier, lambda: Notifier())
+        self.config = map_optional(
+            config_file, lambda v: config_flags.DEFINE_config_file("config", default=v)
+        )
+        if env_file is not None:
+            load_dotenv(env_file, verbose=True)
+
+    def run(self, main: MainFn):
+        @wraps(main)
+        def hook_main(argv):
+            app_name = value_or_default(self.name, argv[0])
+            self.notifier.notify_job_started(app_name)
+            ex_handler = ExceptionHandlerImpl(app_name, self.notifier)
+            app.install_exception_handler(ex_handler)
+            logging.info("-" * 50)
+            logging.info(
+                f"Flags: {json.dumps(flags.FLAGS.flag_values_dict(), sort_keys=True, indent=4)}"
+            )
+            logging.info("-" * 50)
+            if self.config is not None:
+                logging.info(
+                    f"Config: {json.dumps(self.config.value, sort_keys=True, indent=4)}"
+                )
+                logging.info("-" * 50)
+            main(argv, map_optional(self.config, attrgetter("value")))
+            self.notifier.notify_job_finished(app_name)
+
+        app.run(hook_main)
