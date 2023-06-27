2e40c0ee7410f939a4c6c3d7c7ec94c0a9739776
artem-sereda <artem.sereda@campus.tu-berlin.de>
2023-06-27 23:01:39 +0200

    :arrow_up:

--- absl_extra/__init__.py
+++ absl_extra/__init__.py
@@ -6,17 +6,17 @@ def is_lib_installed(name: str) -> bool:
     return util.find_spec(name) is not None
 
 
-from absl_extra.tasks import run, register_task
-from absl_extra.notifier import BaseNotifier
+from absl_extra.src.tasks import run, register_task
+from absl_extra.src.notifier import BaseNotifier
 
 if is_lib_installed("slack_sdk"):
-    from absl_extra.notifier import SlackNotifier
-from absl_extra.logging_utils import log_before, log_after, setup_logging
+    from absl_extra.src.notifier import SlackNotifier
+from absl_extra.src.logging_utils import log_before, log_after, setup_logging
 
 if is_lib_installed("pymongo"):
-    from absl_extra.tasks import MongoConfig
+    from absl_extra.src.tasks import MongoConfig
 if is_lib_installed("tensorflow"):
-    from absl_extra.tf_utils import (
+    from absl_extra.src.tf_utils import (
         supports_mixed_precision,
         make_gpu_strategy,
         make_tpu_strategy,
